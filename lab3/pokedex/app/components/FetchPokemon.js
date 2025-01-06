2517b217d937ea9435a118233cc2011170bc35b7
julnac <julia.naczke007@gmail.com>
2025-01-06 15:57:17 +0100

    add dot

--- lab3/pokedex/app/components/FetchPokemon.js
+++ lab3/pokedex/app/components/FetchPokemon.js
@@ -2,7 +2,7 @@ export default async function fetchPokemons() {
     try {
         const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100');
         if (!response.ok) {
-            throw new Error('Network response was not ok');
+            throw new Error('Network response was not ok.');
         }
         const data = await response.json();
         const pokemonDetails = await Promise.all(
